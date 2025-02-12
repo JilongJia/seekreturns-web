7d0fcabd7a647fc35f1ccf0daab83047f11eb065
Jilong Jia <jilongjia19@gmail.com>
2025-02-12 12:02:06 +0800

    fix cloudbuild

--- app/lib/db/firebaseAdmin.ts
+++ app/lib/db/firebaseAdmin.ts
@@ -1,6 +1,10 @@
 import { getApps, initializeApp, applicationDefault } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-if (!getApps().length) initializeApp({ credential: applicationDefault() });
+if (!getApps().length)
+  initializeApp({
+    credential: applicationDefault(),
+    projectId: "jilongs-gcp-playground",
+  });
 
 export const adminDb = getFirestore();
