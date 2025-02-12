b49e05c41bce6f476ecfb90a464616ee57b41a28
Jilong Jia <jilongjia19@gmail.com>
2025-02-12 22:14:14 +0800

    test firebase connection

--- app/lib/db/firebaseAdmin.ts
+++ app/lib/db/firebaseAdmin.ts
@@ -1,9 +1,6 @@
-import { getApps, initializeApp, applicationDefault } from "firebase-admin/app";
+import { getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-if (!getApps().length)
-  initializeApp({
-    credential: applicationDefault(),
-  });
+if (!getApps().length) initializeApp();
 
 export const adminDb = getFirestore();
