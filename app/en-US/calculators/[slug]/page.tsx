4b663f7ae5097089962d7d0e9a18b18f7be8c32f
Jilong Jia <jilongjia19@gmail.com>
2025-02-21 16:15:59 +0800

    test cloud build

--- app/en-US/calculators/[slug]/page.tsx
+++ app/en-US/calculators/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import dynamic from "next/dynamic";
 import clsx from "clsx";
 
-// import { getStaticParams } from "@/app/lib/db/getStaticParams";
+import { getStaticParams } from "@/app/lib/db/getStaticParams";
 
 import { Header } from "@/app/components/en-US/content/page/Header";
 import { TableOfContentsSidebar } from "@/app/components/en-US/content/page/TableOfContentsSidebar";
@@ -36,13 +36,13 @@ async function Page({ params }: PageProps) {
   );
 }
 
-// export async function generateStaticParams() {
-//   const language = "en-US";
-//   const section = "calculators";
-//   const staticParams = await getStaticParams(language, section);
-//   return staticParams;
-// }
+export async function generateStaticParams() {
+  const language = "en-US";
+  const section = "calculators";
+  const staticParams = await getStaticParams(language, section);
+  return staticParams;
+}
 
-// export const dynamicParams = false;
+export const dynamicParams = false;
 
 export default Page;
