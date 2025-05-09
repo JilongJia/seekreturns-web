a186e0ed166cd388e08f1e3fc0039d3294fba2e4
Jilong Jia <jilongjia19@gmail.com>
2025-05-09 10:57:51 +0800

    add search chinese

---
+++ app/zh/search/page.tsx
@@ -0,0 +1,56 @@
+import clsx from "clsx";
+
+import { generateWebsiteMetadata } from "@/app/lib/en/utility/generateMetadata";
+import { generateJsonLd } from "./lib/generateJsonLd";
+
+import { Header } from "@/app/components/en/utility/page/Header";
+import { Footer } from "@/app/components/en/utility/page/Footer";
+import { Search } from "./components/Search";
+import styles from "./page.module.css";
+
+import { pageInfo } from "./data/info";
+
+export function generateMetadata() {
+  const metadata = generateWebsiteMetadata(pageInfo);
+
+  return metadata;
+}
+
+function Page() {
+  const {
+    title: pageTitle,
+    pathname: pagePathname,
+    description: pageDescription,
+    publishedDate: pagePublishedDate,
+    modifiedDate: pageModifiedDate,
+  } = pageInfo;
+
+  const jsonLd = generateJsonLd({
+    pageTitle,
+    pagePathname,
+    pageDescription,
+    pagePublishedDate,
+    pageModifiedDate,
+  });
+
+  return (
+    <>
+      <Header
+        pathname="/en/search"
+        className={clsx(styles.header, "layoutContainer")}
+      />
+      <main className={styles.main}>
+        <Search />
+      </main>
+      <Footer className={clsx(styles.footer, "layoutContainer")} />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+    </>
+  );
+}
+
+export const dynamic = "force-dynamic";
+
+export default Page;
