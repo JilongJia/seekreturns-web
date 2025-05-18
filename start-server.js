fa2edcc06af292d612ec2afbbc5ea09ef1c3be1d
Jilong Jia <jilongjia19@gmail.com>
2025-05-18 19:25:34 +0800

    fix bugs

--- start-server.js
+++ start-server.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-require("@google-cloud/profiler").start({
-  serviceContext: {
-    service: "seekreturns-web",
-    version: process.env.K_REVISION,
-  },
-});
+// require("@google-cloud/profiler").start({
+//   serviceContext: {
+//     service: "seekreturns-web",
+//     version: process.env.K_REVISION,
+//   },
+// });
 
 require("./server.js");
