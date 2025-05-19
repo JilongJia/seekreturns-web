34d867a5501a956bb812936bdac57ac3f4f0c26c
Jilong Jia <jilongjia19@gmail.com>
2025-05-19 10:50:17 +0800

    fix bugs

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  outputFileTracingIncludes: {
-    "*": ["./node_modules/@google-cloud/profiler/**/*"],
-  },
   async redirects() {
     return [
       {
