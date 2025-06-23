60b0aa777366a4fa9f297c1486553278e0cdf378
Jilong Jia <jilongjia19@gmail.com>
2025-06-24 00:12:23 +0800

    update robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,21 @@
 import type { MetadataRoute } from "next";
 
 function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
   return {
     rules: {
       userAgent: "*",
       allow: "/",
       disallow: ["/go/", "/en/search/", "/ja/search/", "/zh/search/"],
     },
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+    sitemap: [
+      `${baseUrl}/sitemap/0.xml`,
+      `${baseUrl}/sitemap/1.xml`,
+      `${baseUrl}/sitemap/2.xml`,
+      `${baseUrl}/sitemap/3.xml`,
+      `${baseUrl}/sitemap/4.xml`,
+    ],
   };
 }
 
